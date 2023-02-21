3eca39e5a209e4366008a429426ad346adb05ae1
gabrieldelpacheco182 <gabriel_o_pacheco@estudante.sesisenai.org.br>
2023-02-21 12:25:34 -0300

    adicionado imagem, favicon, emoji e algumas alterações no css.

--- function.js
+++ function.js
@@ -1,13 +1,11 @@
 /* let numero = document.getElementById("numero").value;
 let texto = document.getElementById("texto").value; */
 
-function gerarLink () {
-let numero = document.getElementById("numero").value;
-let texto = document.getElementById("texto").value;
-let link = `https://wa.me/send?phone=${numero}&text=${texto}`
+function gerarLink() {
+  let numero = document.getElementById("numero").value;
+  let texto = document.getElementById("texto").value;
+  let link = `https://wa.me/send?phone=${numero}&text=${texto}`;
 
-/*innerHTML pegar tag chama atrasves do id (''), e substituir por um nova tag `<a>`*/
-document.getElementById('urlFinal').innerHTML = `<a href="${link}" target="_blank">Link gerado para enviar whatsapp</a>`
-};
-
-
+  /*innerHTML pegar tag chama atrasves do id (''), e substituir por um nova tag `<a>`*/
+  document.getElementById("urlFinal").innerHTML = `<a href="${link}" target="_blank">Abra aqui o whatsapp</a>`;
+}
